1ee31ddda4dc17e93c39bdb8cb1fa70595af1549
Rachel Barker <rachelbarker001@gmail.com>
2018-06-02 01:16:44 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log("Up and running!");
+
+// give values to cards
+// two queen cards
+// two king cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+// display two cards
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
