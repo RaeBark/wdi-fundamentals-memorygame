95cfd10aa73519b101884673aae558654f410aff
Rachel Barker <rachelbarker001@gmail.com>
2018-06-02 22:19:17 -0700

    added the real game logic this time

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-console.log("Up and running!");
 
-// give values to cards
-// two queen cards
-// two king cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-// display two cards
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+// assign 1st card to cardOne
+var cardOne = cards[0];
+// add cardOne to cardsInPlay
+cardsInPlay.push(cardOne);
+console.log('User filpped queen.');
+
+// assign 3rd card to cardTwo
+var cardTwo = cards[2];
+// add cardTwo to cardsInPlay
+cardsInPlay.push(cardTwo);
+console.log('User flipped king.');
+
+//check to see if there are 2 cards in play
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again');
+	}
+}
