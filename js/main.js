5b94b757661d430ed5a0b6feb3551c37ed7d25b8
Rachel Barker <rachelbarker001@gmail.com>
2018-06-03 15:39:41 -0700

    added function for flipCard and checkMatch

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-// assign 1st card to cardOne
-var cardOne = cards[0];
-// add cardOne to cardsInPlay
-cardsInPlay.push(cardOne);
-console.log('User filpped queen.');
-
-// assign 3rd card to cardTwo
-var cardTwo = cards[2];
-// add cardTwo to cardsInPlay
-cardsInPlay.push(cardTwo);
-console.log('User flipped king.');
-
-//check to see if there are 2 cards in play
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} else {
-		alert('Sorry, try again');
+//checks  to see if there are 2 cards in play
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log('You found a match!');
+		} else {
+			console.log('Sorry, try again');
+		}
 	}
-}
+};
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
