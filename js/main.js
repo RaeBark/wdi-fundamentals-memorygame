e1abefe639d45dd54a64fb581eb01b8f441402b4
Rachel Barker <rachelbarker001@gmail.com>
2018-06-03 18:58:54 -0700

    added more game logic again

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "Hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "Diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "Hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank :"King",
+		suit: "Diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
 //checks  to see if there are 2 cards in play
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			console.log('You found a match!');
+			alert('You found a match!');
 		} else {
-			console.log('Sorry, try again');
+			alert('Sorry, try again');
 		}
 	}
 };
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit)
 
 	checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
+
